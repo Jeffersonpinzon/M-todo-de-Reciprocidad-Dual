1e15ac3cf14207de5307efdc135662a9fe5edf86
Jefferson Pinzon <83316586+Jeffersonpinzon@users.noreply.github.com>
2021-04-29 15:26:40 -0500

    Delete Gauss_Legendre.m

--- Gauss_Legendre.m
+++
@@ -1,6 +0,0 @@
-function I=Gauss_Legendre(f,a,b,N,varargin)
-%Gauss_Legendre integration of f over [a,b] with N grid points
-% Never try N larger than 25
-[t,w]=Gausslp(N); x=((b-a)*t+a+b)/2; % Eq.(5.9.9)
-fx=feval(f,x,varargin{:});
-I=w*fx'*(b-a)/2; % Eq.(5.9.10)
